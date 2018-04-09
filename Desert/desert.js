4dd84f0820baf3ee3ddf00a4caec7060cb5d9578
Idea456 <riowongsoatmojo@gmail.com>
2018-04-09 17:20:04 +0700

    Update desert.js

--- Desert/desert.js
+++ Desert/desert.js
@@ -1,11 +1,10 @@
-//Desert program from Computer Science A level 9608_w17_qp_41 Q6
-//By Adrian Rio
-
 var animals = []
-var animalCount = 10;
-
+//change the animal count here
+var animalCount = 20;
+var score = 0;
+ 
 function setup() {
-  createCanvas(1600, 1600);
+  createCanvas(2000,2000);
   board = new Board();
   food = new Food();
   for (i = 0; i <= animalCount; i++) {
@@ -13,24 +12,28 @@ function setup() {
     animals.push(animal);
   }
 }
-
+ 
 function draw() {
-  frameRate(1);
+  //change the speed here
+  frameRate(30);
   background(255);
   board.show();
   food.show();
   for (i = 0; i < animals.length; i++) {
     animals[i].show();
     animals[i].move();
+    food.generateFood(animals[i].x,animals[i].y);
+    animals[i].displayScore();
   }
+ 
 }
 
 function Board() {
   this.size = 40;
-
+ 
   this.show = function() {
-    for (i = 0; i <= height; i++) {
-      for (j = 0; j <= width; j++) {
+    for (i = 0; i <= 1601; i++) {
+      for (j = 0; j <= 1601; j++) {
         fill(255);
         rect(i, j, this.size, this.size);
         j += this.size - 1;
@@ -39,14 +42,14 @@ function Board() {
     }
   }
 }
-
+  
 function Animal() {
   //generate a pair of random numbers between 0 and 39
   this.x = byte(random(39)) * 40;
   this.y = byte(random(39)) * 40;
   this.xspeed = 40;
   this.score = 0;
-
+ 
   //place animal at that random position
   this.show = function() {
     fill(255, 0, 0);
@@ -55,25 +58,51 @@ function Animal() {
     fill(0);
     text("A", this.x + 10, this.y + 30);
   }
-
+ 
   this.move = function() {
     //GenerateChangeInCoordinate function 
     this.xspeed = byte(random([-1,0,1]) * 40);
     this.yspeed = byte(random([-1,0,1]) * 40);
+    check = false;
+    while(check != true) {
+      if((this.x + this.xspeed >= 1601) || (this.y + this.yspeed >= 1601)) {
+        this.xspeed = byte(random([-1,0,1]) * 40);
+        this.yspeed = byte(random([-1,0,1]) * 40);
+      } else if((this.x + this.xspeed <= 0) || (this.y + this.yspeed <= 0)) {
+				this.xspeed = byte(random([-1,0,1]) * 40);
+        this.yspeed = byte(random([-1,0,1]) * 40);
+			} else {
+        check = true;
+      }
+    }
     this.x += this.xspeed;
     this.y += this.yspeed;
   }
+	
+  this.displayScore = function() {
+    text("Animal Score",1700,40);
+    text(score,1780,80);
+  }
 }
-
+ 
 function Food() {
   this.x = byte(random(39)) * 40;
   this.y = byte(random(39)) * 40;
-
+	
   this.show = function() {
     fill(255, 0, 255);
     rect(this.x, this.y, 40, 40);
     textSize(30);
     fill(0);
     text("F", this.x + 10, this.y + 30);
+  }
+
+  this.generateFood = function(x,y) {
+    if((x == this.x) && (y == this.y)) {
+    this.x = byte(random(39)) * 40;
+    this.y = byte(random(39)) * 40;
+    score += 1;
+    console.log("Food eaten!");
+    }
   }
 }
